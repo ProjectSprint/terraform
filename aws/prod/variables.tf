a1c41bb1282a3038fedda724e08af449173ca8de
nandanugg <nandanugg@gmail.com>
2025-01-22 12:13:00 +0700

    remove unused variables

--- aws/prod/variables.tf
+++ aws/prod/variables.tf
@@ -4,10 +4,6 @@ locals {
   s3_backend_name = "projectsprint-tf-state"
 }
 
-variable "subnet_1a" {}
-variable "subnet_1b" {}
-variable "subnet_1c" {}
-
 variable "projectsprint_vm_public_key" {
   type    = string
   default = ""
