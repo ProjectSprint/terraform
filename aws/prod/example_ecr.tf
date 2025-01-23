7b282a4533f57330710fcea756adc67ff9bd7c30
nandanugg <nandanugg@gmail.com>
2025-01-23 07:22:02 +0700

    fix

--- aws/prod/example_ecr.tf
+++ aws/prod/example_ecr.tf
@@ -1,53 +1,5 @@
-# https://registry.terraform.io/modules/terraform-aws-modules/iam/aws/latest/submodules/iam-policy
-module "ecr_policy_example" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-policy"
-  version = "5.37.1"
-
-  name = "projectsprint-ecr-example" # Fixed typo in name
-  path = "/"
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "ecr:DescribeRepositories",
-          "ecr:GetRegistryScanningConfiguration",
-          "ecr:GetAuthorizationToken"
-        ]
-        Resource = "*"
-      },
-      {
-        Effect = "Allow"
-        Action = [
-          "ecr:*"
-        ]
-        Resource = [
-          module.ecr_example.repository_arn
-        ]
-      },
-      {
-        Effect = "Deny"
-        Action = [
-          "ecr:DeleteRepository",
-          "ecr:DeleteRepositoryPolicy",
-          "ecr:PutImageTagMutability",
-          "ecr:PutImageScanningConfiguration"
-        ]
-        Resource = "*"
-      }
-    ]
-  })
-}
-
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_user_policy_attachment
-resource "aws_iam_user_policy_attachment" "projectsprint_ecr" {
-  user       = module.projectsprint_iam_account["example"].iam_user_name
-  policy_arn = module.ecr_policy_example.arn
-}
-
 # https://registry.terraform.io/modules/terraform-aws-modules/ecr/aws/latest
-module "ecr_example" {
+module "example_ecr" {
   source = "terraform-aws-modules/ecr/aws"
 
   repository_name                   = "example-repository"
