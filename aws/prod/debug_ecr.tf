badad6958b96bfb1acbdad302b83efba3443f4d3
Kenji Hikmatullah <43457338+kenjihikmatullah@users.noreply.github.com>
2025-01-26 19:25:34 +0700

    [Dev][Debug] Infra Provisioning (#8)
    Co-authored-by: Nanda Nugraha <nandanugg@gmail.com>

---
+++ aws/prod/debug_ecr.tf
@@ -0,0 +1,34 @@
+# https://registry.terraform.io/modules/terraform-aws-modules/ecr/aws/latest
+module "debug_ecr" {
+  source = "terraform-aws-modules/ecr/aws"
+
+  for_each = toset(var.debug_services)
+
+  repository_name                   = "debug-${each.value}"
+  repository_type                   = "public"
+  repository_image_tag_mutability   = "MUTABLE"
+  repository_force_delete           = true
+  repository_read_write_access_arns = [module.projectsprint_iam_account["debug"].iam_user_arn]
+  repository_lifecycle_policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1,
+        description  = "Keep last 10 images",
+        selection = {
+          tagStatus     = "tagged",
+          tagPrefixList = ["v"],
+          countType     = "imageCountMoreThan",
+          countNumber   = 10
+        },
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+
+  tags = {
+    project = "projectsprint",
+    Name    = "debug-db"
+  }
+}
