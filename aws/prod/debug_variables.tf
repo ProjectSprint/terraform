173a49f4a81e9f7d0461361b03438587582423c6
nandawartek <nanda@wartek.belajar.id>
2025-01-27 10:35:20 +0700

    updae
    Signed-off-by: nandawartek <nanda@wartek.belajar.id>

--- aws/prod/debug_variables.tf
+++ aws/prod/debug_variables.tf
@@ -12,16 +12,6 @@ variable "debug_service_configs" {
   }
 }
 
-# Prod
-# variable "debug_service_configs" {
-#   default = {
-#     "user-service"     = { container_port = 8081, cpu = 1024, memory = 4096, path_pattern = "/v1/user/*", instance_count = 1 }
-#     "product-service"  = { container_port = 8082, cpu = 1024, memory = 4096, path_pattern = "/v1/product/*", instance_count = 2 }
-#     "purchase-service" = { container_port = 8083, cpu = 1024, memory = 4096, path_pattern = "/v1/purchase/*", instance_count = 1 }
-#     "file-service"     = { container_port = 8084, cpu = 1024, memory = 4096, path_pattern = "/v1/file/*", instance_count = 1 }
-#   }
-# }
-
 variable "debug_databases" {
   default = ["debug-user-service-db", "debug-product-service-db", "debug-purchase-service-db", "debug-file-service-db"]
 }
@@ -37,6 +27,15 @@ variable "debug_database_configs" {
 }
 
 # Prod
+# variable "debug_service_configs" {
+#   default = {
+#     "user-service"     = { container_port = 8081, cpu = 1024, memory = 4096, path_pattern = "/v1/user/*", instance_count = 1 }
+#     "product-service"  = { container_port = 8082, cpu = 1024, memory = 4096, path_pattern = "/v1/product/*", instance_count = 2 }
+#     "purchase-service" = { container_port = 8083, cpu = 1024, memory = 4096, path_pattern = "/v1/purchase/*", instance_count = 1 }
+#     "file-service"     = { container_port = 8084, cpu = 1024, memory = 4096, path_pattern = "/v1/file/*", instance_count = 1 }
+#   }
+# }
+# Prod
 # variable "debug_database_configs" {
 #   default = {
 #     "debug-user-service-db"     = { instance_type = "db.t4g.medium", db_name = "users" }
@@ -45,3 +44,4 @@ variable "debug_database_configs" {
 #     "debug-file-service-db"     = { instance_type = "db.t4g.small", db_name = "files" }
 #   }
 # }
+
