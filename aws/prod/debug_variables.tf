8b97845836e8e14f6d1beda484448b4f1db81c5f
nandawartek <nanda@wartek.belajar.id>
2025-01-27 11:03:15 +0700

    change debug team availability

--- aws/prod/debug_variables.tf
+++ aws/prod/debug_variables.tf
@@ -5,10 +5,10 @@ variable "debug_services" {
 # Dev
 variable "debug_service_configs" {
   default = {
-    "user-service"     = { container_port = 8081, cpu = 256, memory = 512, path_pattern = "/v1/user/*", instance_count = 1 }
-    "product-service"  = { container_port = 8082, cpu = 256, memory = 512, path_pattern = "/v1/product/*", instance_count = 2 }
-    "purchase-service" = { container_port = 8083, cpu = 256, memory = 512, path_pattern = "/v1/purchase/*", instance_count = 1 }
-    "file-service"     = { container_port = 8084, cpu = 256, memory = 512, path_pattern = "/v1/file/*", instance_count = 1 }
+    "user-service"     = { container_port = 8081, cpu = 256, memory = 512, path_pattern = "/v1/user/*", instance_count = 0 }
+    "product-service"  = { container_port = 8082, cpu = 256, memory = 512, path_pattern = "/v1/product/*", instance_count = 0 }
+    "purchase-service" = { container_port = 8083, cpu = 256, memory = 512, path_pattern = "/v1/purchase/*", instance_count = 0 }
+    "file-service"     = { container_port = 8084, cpu = 256, memory = 512, path_pattern = "/v1/file/*", instance_count = 0 }
   }
 }
 
