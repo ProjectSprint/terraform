4b46e15434aeebb7a76325978532ed32b357f96d
Manda Putra <mandaputra8@gmail.com>
2025-02-07 13:25:26 +0700

    Feature/[malu-tapi-suhu] infrastructure request for development (#9)

---
+++ aws/prod/malutapisuhu_variables.tf
@@ -0,0 +1,11 @@
+variable "malutapisuhu_databases" {
+  default = "malutapisuhu-db"
+}
+
+variable "malutapisuhu_database_configs" {
+  default = {
+    name          = "malutapisuhu-db",
+    instance_type = "db.t4g.micro",
+    db_name       = "malutapisuhu-tutuplapak"
+  }
+}
