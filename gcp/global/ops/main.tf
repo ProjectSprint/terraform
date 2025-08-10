1ea17b32c518fe15fcada868c8cca22100f6e367
nandanugg <nandanugg@gmail.com>
2025-08-11 02:42:08 +0700

    update

--- gcp/global/ops/main.tf
+++ gcp/global/ops/main.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+  }
+
+  backend "gcs" {
+    bucket = "projectsprint-tf-backend"
+    prefix = "terraform/state"
+  }
+}
+
 provider "google" {
   project = var.google_project_id
   # pricing comparison (e2-micro, spot) 9 Jun 2025
