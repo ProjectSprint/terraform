dc561da007851af723bb587e4d878908d1eb1b46
nandanugg <nandanugg@gmail.com>
2025-11-02 08:05:49 +0700

    checkpoint

--- ps/environments/cluster1/main.tf
+++ ps/environments/cluster1/main.tf
@@ -1,10 +1,10 @@
 terraform {
-  # backend "kubernetes" {
-  #   secret_suffix     = "state"
-  #   namespace         = "tf-state"
-  #   in_cluster_config = true
-  #   config_path       = "admin.kubeconfig"
-  # }
+  backend "kubernetes" {
+    secret_suffix = "state"
+    namespace     = "tf-state"
+    # in_cluster_config = true
+    config_path = "~/.kube/ps.kubeconfig"
+  }
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
@@ -18,12 +18,10 @@ terraform {
 }
 
 provider "kubernetes" {
-  config_path = var.kubeconfig
 }
 
 provider "helm" {
   kubernetes = {
-    config_path = var.kubeconfig
   }
 }
 
@@ -41,14 +39,6 @@ module "grafana_tls" {
   namespace   = module.namespaces.monitoring_name
   domain      = local.grafana_domain
   secret_name = "${local.grafana_domain}-tls"
-  issuer_name = module.cert_manager.issuer_name
-}
-
-module "argocd_tls" {
-  source      = "../../modules/tls-certificate"
-  namespace   = "argocd"
-  domain      = "argocd-cluster1.projectsprint.id"
-  secret_name = "argocd-server-tls"
   issuer_name = module.cert_manager.issuer_name
 }
 
@@ -83,14 +73,5 @@ module "grafana_ingress" {
   service_namespace       = module.namespaces.monitoring_name
   service_tls_secret_name = module.grafana_tls.secret_name
   service_name            = module.monitoring.grafana_svc_name
-  service_port            = 80
-}
-
-module "argocd_ingress" {
-  source                  = "../../modules/ingress"
-  domain                  = "argocd-cluster1.projectsprint.id"
-  service_namespace       = "argocd"
-  service_tls_secret_name = module.argocd_tls.secret_name
-  service_name            = "argocd-server"
   service_port            = 80
 }
--- ps/modules/namespaces/main.tf
+++ ps/modules/namespaces/main.tf
@@ -4,7 +4,6 @@ resource "kubernetes_namespace" "monitoring" {
   }
 }
 
-# terraform import 'module.namespaces.kubernetes_namespace.kube_system' 'kube-system'
 resource "kubernetes_namespace" "kube_system" {
   metadata {
     name = "kube-system"
